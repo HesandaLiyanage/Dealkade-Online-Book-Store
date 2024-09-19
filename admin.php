f23b402e363abad3c3ba287ede812660e10a6bce
vijiniLM <jini.a.lm2@gmail.com>
2024-09-18 21:27:22 -0700

    Update admin.php :added a js part

--- admin.php
+++ admin.php
@@ -48,7 +48,7 @@ $result_users = $conn->query($sql_users);
                                 <td>{$user['name']}</td>
                                 <td>{$user['address']}</td>
                                 <td>{$user['phone_number']}</td>
-                                <td><a href='viewUser.php?id={$user['id']}'>View Details</a></td>
+                                <td><a href='viewUser.php?id={$user['id']}' onclick='view()'>View Details</a></td>
                               </tr>";
                     }
                 } else {
@@ -58,6 +58,15 @@ $result_users = $conn->query($sql_users);
             </tbody>
         </table>
     </div>
+  <script>
+        
+
+        function view()
+        {
+            alert("Do you want to view the User ?");
+        }
+    </script>
+    
 </body>
 </html>
 
